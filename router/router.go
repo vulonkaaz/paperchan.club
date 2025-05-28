c7f9c3edb05ab8dc1819c9686e7a6c4d60c1ba15
vulonkaaz <7442677+vulonkaaz@users.noreply.github.com>
2025-05-28 20:44:40 +0200

    rate limiting

--- router/router.go
+++ router/router.go
@@ -3,11 +3,22 @@ package router
 import (
 	"paperchan.club/controllers"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"time"
+	"strings"
 )
 
 func SetRoutes(app *fiber.App) {
 	app.Get("/", controllers.ThreadList)
 	app.Get("/thread/:id", controllers.Thread)
-	app.Post("/api/post", controllers.Publish)
+	app.Post("/api/post", rateLimiter, controllers.Publish)
 	app.Delete("/api/post", controllers.Delete)
 }
+
+var rateLimiter = limiter.New(limiter.Config{
+	Max: 5,
+	Expiration: 10*time.Minute,
+	KeyGenerator: func(c *fiber.Ctx) string {
+		return c.IP()+" "+strings.Join(c.IPs()," ")
+	},
+})
