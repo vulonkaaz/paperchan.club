d1fa0d22af1fb08246f82cba04a8828d59dc07ab
vulonkaaz <7442677+vulonkaaz@users.noreply.github.com>
2026-08-10 12:13:17 +0200

    new banners + about page

--- controllers/pages.go
+++ controllers/pages.go
@@ -9,7 +9,7 @@ import (
 )
 
 const maxThreadsPerPage = 24
-const maxBanner = 24 // dont forget to increment whenever we add more
+const maxBanner = 32 // dont forget to increment whenever we add more
 
 func ThreadList(c *fiber.Ctx) error {
 	db := database.DB
